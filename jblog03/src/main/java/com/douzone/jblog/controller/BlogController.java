1e01e32c0b971da6572d59ecf5ad4f346eec6487
hwimin-kim <gnlals7841@gmail.com>
2022-06-17 16:13:59 +0900

    jblog #3

--- jblog03/src/main/java/com/douzone/jblog/controller/BlogController.java
+++ jblog03/src/main/java/com/douzone/jblog/controller/BlogController.java
@@ -5,6 +5,33 @@ package com.douzone.jblog.controller;
 //kickscar/admin/
 //kickscar/admin/write
 //kickscar/admin/category
-public class BlogController {
 
+/*@Controller
+
+@RequestMapping("/{id:(?!assets).*}")*/
+public class BlogController {
+	/*
+	 * @RequestMapping({"", "/{pathNo1}", "/{pathNo1}/{pathNo2}"}) public String
+	 * index(
+	 * 
+	 * @PathVariable("id") String id,
+	 * 
+	 * @PathVariable("pathNo1") Optional<Long> pathNo1,
+	 * 
+	 * @PathVariable("pathNo1") Optional<Long> pathNo2) {
+	 * 
+	 * Long categoryNo = 0L; Long postNo = 0L;
+	 * 
+	 * if(pathNo2.isPresent()) { categoryNo = pathNo1.get(); postNo = pathNo2.get();
+	 * } else if(pathNo1.isPresent()) { categoryNo = pathNo1.get(); }
+	 * 
+	 * categoryService.getCategories(id, categoryNo)
+	 * 
+	 * }
+	 * 
+	 * @RequestMapping("/admin/basic") public String adminBasic(@PathVariable("id")
+	 * String id) {
+	 * 
+	 * }
+	 */
 }
