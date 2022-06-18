00f303bfc9c6ce7c45b872706dfed7da09669243
hwimin-kim <gnlals7841@gmail.com>
2022-06-18 16:04:44 +0900

    기타...

--- jblog03/src/main/java/com/douzone/jblog/controller/UserController.java
+++ jblog03/src/main/java/com/douzone/jblog/controller/UserController.java
@@ -1,5 +1,47 @@
 package com.douzone.jblog.controller;
 
-public class UserController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.douzone.jblog.service.UserService;
+import com.douzone.jblog.vo.UserVo;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+	@Autowired
+	private UserService userService;
+	
+	@RequestMapping("/login")
+	public String login() {
+		return "user/login";
+	}
+	
+	@RequestMapping("/auth")
+	public void auth() {
+	}
+	
+	@RequestMapping("/logout")
+	public void logout() {
+	}	
+	
+	@RequestMapping(value="/join", method=RequestMethod.GET)
+	public String join() {
+		return "user/join";
+	}
+	
+	@RequestMapping(value="/join", method=RequestMethod.POST)
+	public String join(@ModelAttribute("userVo") UserVo userVo) {
+		userService.join(userVo);
+		
+		return "redirect:/user/joinsuccess";
+	}
+		
+	@RequestMapping("/joinsuccess")
+	public String joinsuccess() {
+		return "user/joinsuccess";
+	}
 }
